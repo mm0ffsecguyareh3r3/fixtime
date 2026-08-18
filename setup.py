0d700f967ec80fbf0f32d3cf5201079a5e022324
b0b0aung <160343524+boboaung1337@users.noreply.github.com>
2026-08-18 14:02:39 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="fixtime",
+    version="2.0.0",
+    py_modules=["fixtime"],
+    install_requires=[
+        "ntplib",
+        "click",
+        "python-dateutil",
+        "requests"
+    ],
+    entry_points={
+        "console_scripts": [
+            "fixtime=fixtime:main",
+        ],
+    },
+    author="watcher1337",
+    description="Time synchronization tool for Kerberos authentication",
+    url="https://github.com/watcher1337/fixtime",
+    python_requires=">=3.8",
+)
